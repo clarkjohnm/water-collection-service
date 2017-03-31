f490cc15c31b9cda12e33e8369edc420a8362733
John Clark <johnclark@paypal.com>
2017-03-31 14:19:55 -0400

    Initial project structure and resource model

---
+++ water-collection-service-impl/src/main/java/org/cybersapien/service/watercollection/WaterCollectionServiceApplication.java
@@ -0,0 +1,12 @@
+package org.cybersapien.service.watercollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterCollectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterCollectionServiceApplication.class, args);
+	}
+}
---
+++ water-collection-service-impl/src/test/java/org/cybersapien/service/watercollection/WaterCollectionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cybersapien.service.watercollection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WaterCollectionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
