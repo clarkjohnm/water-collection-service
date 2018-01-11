620495c0d553d5faf87346c13e8018a4e8657ca4
johnclark <johnclark@paypal.com>
2018-01-10 21:18:29 -0500

    Dynamically add users

--- water-collection-service-impl/src/main/java/org/cybersapien/watercollection/config/SecurityConfig.java
+++ water-collection-service-impl/src/main/java/org/cybersapien/watercollection/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package org.cybersapien.watercollection.config;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,17 +24,39 @@ public class SecurityConfig {
     @Configuration
     @RequiredArgsConstructor
     @EncryptablePropertySource("classpath:credentials.yml")
+    @Slf4j
     public static class ApiSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
         /**
          * The Spring Environment
          */
-        private final Environment environment;
+        private final ConfigurableEnvironment configurableEnvironment;
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            // Add users
-            auth.inMemoryAuthentication().withUser("wcsuser").password(environment.getProperty("credentials.wcsuser.password")).roles("USER");
-            auth.inMemoryAuthentication().withUser("wcsadmin").password(environment.getProperty("credentials.wcsadmin.password")).roles("ADMIN", "ACTUATOR");
+            // Dynamically add users by iterating over the properties and finding the credentials property that ends with "roles"
+            MutablePropertySources mutablePropertySources = configurableEnvironment.getPropertySources();
+            for (PropertySource<?> propertySource : mutablePropertySources) {
+                if (propertySource instanceof EnumerablePropertySource) {
+                    EnumerablePropertySource enumerablePropertySource = (EnumerablePropertySource) propertySource;
+                    String[] propertyNames = enumerablePropertySource.getPropertyNames();
+                    for (int i = 0; i < propertyNames.length; ++i) {
+                        if (propertyNames[i].startsWith("credentials") && propertyNames[i].endsWith("roles")) {
+                            String[] nameComponents = propertyNames[i].split("\\.");
+                            if (nameComponents.length == 3) {
+                                String userId = nameComponents[1];
+                                String password = configurableEnvironment.getProperty("credentials." + userId + ".password");
+                                String roles = configurableEnvironment.getProperty("credentials." + userId + ".roles");
+
+                                if ((userId != null) && (password != null) && (roles != null)) {
+                                    auth.inMemoryAuthentication().withUser(userId).password(password).roles(roles.split(","));
+                                } else {
+                                    log.error("Credentials cannot be added for " + userId);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         @Override
