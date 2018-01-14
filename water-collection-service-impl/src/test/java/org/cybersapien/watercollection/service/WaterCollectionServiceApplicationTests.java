f7180a0d8312da0b05c2e0c85e2d7d142d48e75d
johnclark <johnclark@paypal.com>
2018-01-14 16:06:14 -0500

    Switch from junit to testng

--- water-collection-service-impl/src/test/java/org/cybersapien/watercollection/service/WaterCollectionServiceApplicationTests.java
+++ water-collection-service-impl/src/test/java/org/cybersapien/watercollection/service/WaterCollectionServiceApplicationTests.java
@@ -1,13 +1,11 @@
 package org.cybersapien.watercollection.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class WaterCollectionServiceApplicationTests {
+public class WaterCollectionServiceApplicationTests  extends AbstractTestNGSpringContextTests {
 
 	@Test
 	public void contextLoads() {
